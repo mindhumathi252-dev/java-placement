9e87b545947da0a8ce469d9aaf1cf50a9dc415e0
mindhumathi252-dev <mindhumathi252@gmail.com>
2026-08-20 18:11:54 +0530

    SumDigit

---
+++ SumDigits.java
@@ -0,0 +1,17 @@
+// 19_SumDigits.java
+import java.util.Scanner;
+
+class SumDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Math.abs(sc.nextInt());
+        int sum = 0;
+
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+
+        System.out.println(sum);
+    }
+}
