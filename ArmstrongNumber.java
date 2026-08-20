53679228ec91a9763119b10ff23ae652049a3251
mindhumathi252-dev <mindhumathi252@gmail.com>
2026-08-20 11:45:35 +0530

    ArmstrongNumber

---
+++ ArmstrongNumber.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int temp = n;
+        int digits = 0;
+        int sum = 0;
+
+        while (temp != 0) {
+            digits++;
+            temp /= 10;
+        }
+
+        temp = n;
+
+        while (temp != 0) {
+            int digit = temp % 10;
+            sum += (int) Math.pow(digit, digits);
+            temp /= 10;
+        }
+
+        if (sum == n)
+            System.out.println("Armstrong Number");
+        else
+            System.out.println("Not Armstrong Number");
+    }
+}
