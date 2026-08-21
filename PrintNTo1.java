602dddc9c2ec972a715744615191a134ceea7013
mindhumathi252-dev <mindhumathi252@gmail.com>
2026-08-21 20:42:19 +0530

    PrintNTo1

---
+++ PrintNTo1.java
@@ -0,0 +1,12 @@
+// 32_PrintNTo1.java
+import java.util.Scanner;
+
+class PrintNTo1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = n; i >= 1; i--)
+            System.out.print(i + " ");
+    }
+}
